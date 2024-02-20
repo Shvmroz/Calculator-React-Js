3f1c40f8b1680a9c93f370b24d77961d9784b301
Shvmroz <oh.buny@gmail.com>
2024-02-20 12:23:45 +0500

    commit

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -9,17 +9,17 @@ const Calculator = () => {
     const [input, setInput] = useState('');
     const [result, setResult] = useState('');
     const [history, setHistory] = useState([]);
-
+    // handle dark mode =================================
     const [darkMode, setDarkMode] = useState(false);
     const toggleDarkMode = () => {
         setDarkMode(!darkMode);
     };
-
+    // handle button clicks =================================================================
     const handleButtonClick = (value) => {
         // Check if the last input was an operator
         const lastInputWasOperator = /[+\-*/]$/.test(input);
         if (lastInputWasOperator && /[+\-*/]/.test(value)) {
-            // If the last input was an operator and the new input is also an operator, do nothing
+            // If the last input was an operator and the new input is also an operator, Return nothing
             return;
         }
         if (result && !isNaN(value)) {
@@ -28,25 +28,18 @@ const Calculator = () => {
         } else if (result && isNaN(value) && value !== '.') {
             setInput(result + value);
             setResult('');
-        } else if (value === '%') {
-            try {
-                const percentage = parseFloat(input) / 100;
-                setInput(percentage.toString());
-            } catch (error) {
-                setInput('Error');
-            }
         } else {
             setInput(input + value);
         }
     };
-    
+ 
+    // handeling function for calculating =============================
     const handleCalculate = () => {
         try {
             const expression = input.replace(/[^-()\d/*+.]/g, '');
             let newResult = math.evaluate(expression);
-            // Round the result to two decimal places
+            // Round the result to two decimal places after the decimal point
             newResult = Math.round(newResult * 100) / 100;
-    
             const historyItem = `${input} = ${newResult}`;
             setResult(newResult.toString());
             setHistory([...history, historyItem]);
@@ -54,21 +47,19 @@ const Calculator = () => {
             setResult('Error');
         }
     };
-    
-
+    // clear the history ================================================
     const handleClear = () => {
         setInput('');
         setResult('');
         setHistory([]);
     };
-
     const handleBackspace = () => {
         setInput(input.slice(0, -1));
     };
-
+    // Keypress event, handle ==========================================
     const handleKeyboardInput = (event) => {
         const keyValue = event.key;
-        if (/[0-9.+\-*/%]/.test(keyValue)) {
+        if (/[0-9.+\-*/]/.test(keyValue)) {
             handleButtonClick(keyValue);
         } else if (keyValue === 'Enter' || keyValue === '=') {
             handleCalculate();
@@ -81,7 +72,6 @@ const Calculator = () => {
 
     useEffect(() => {
         document.addEventListener('keydown', handleKeyboardInput);
-
         return () => {
             document.removeEventListener('keydown', handleKeyboardInput);
         };
@@ -117,33 +107,32 @@ const Calculator = () => {
                 <div className="buttons">
                     <table>
                         <tr>
-                            <td><button onClick={handleClear} style={{ backgroundColor: 'orange' }}>C</button></td>
-                            <td><button onClick={handleBackspace} className="backspace">DEL</button></td>
-                            <td><button onClick={() => handleButtonClick('%')}>%</button></td>
-                            <td><button onClick={() => handleButtonClick('/')}>÷ /</button></td>
+                            <td colSpan='2'><button className='o-clr' style={{width: '112px'}} onClick={handleClear}>C</button></td>
+                            <td><button className="backspace o-clr" onClick={handleBackspace} >DEL</button></td>
+                            <td><button className='o-clr' onClick={() => handleButtonClick('/')}>÷ /</button></td>
                         </tr>
                         <tr>
                             <td><button onClick={() => handleButtonClick('7')}>7</button></td>
                             <td><button onClick={() => handleButtonClick('8')}>8</button></td>
                             <td><button onClick={() => handleButtonClick('9')}>9</button></td>
-                            <td><button onClick={() => handleButtonClick('*')}>x</button></td>
+                            <td><button className='o-clr' onClick={() => handleButtonClick('*')}>x</button></td>
                         </tr>
                         <tr>
                             <td><button onClick={() => handleButtonClick('4')}>4</button></td>
                             <td><button onClick={() => handleButtonClick('5')}>5</button></td>
                             <td><button onClick={() => handleButtonClick('6')}>6</button></td>
-                            <td><button onClick={() => handleButtonClick('-')}>-</button></td>
+                            <td><button className='o-clr' onClick={() => handleButtonClick('-')}>-</button></td>
                         </tr>
                         <tr>
                             <td><button onClick={() => handleButtonClick('1')}>1</button></td>
                             <td><button onClick={() => handleButtonClick('2')}>2</button></td>
                             <td><button onClick={() => handleButtonClick('3')}>3</button></td>
-                            <td><button onClick={() => handleButtonClick('+')}>+</button></td>
+                            <td><button className='o-clr' onClick={() => handleButtonClick('+')}>+</button></td>
                         </tr>
                         <tr>
-                            <td><button onClick={() => handleButtonClick('0')}>0</button></td>
+                            <td colSpan="2"><button style={{width: '112px' }} onClick={() => handleButtonClick('0')}>0</button></td>
                             <td><button onClick={() => handleButtonClick('.')}>.</button></td>
-                            <td colSpan="2"><button style={{ backgroundColor: 'orange', width: '112px' }} onClick={handleCalculate}>=</button></td>
+                            <td><button style={{backgroundColor: 'orange'}} onClick={handleCalculate}>=</button></td>
                         </tr>
                     </table>
                 </div>
